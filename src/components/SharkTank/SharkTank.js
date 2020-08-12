3444da0b52d338fed8cadd18355e3162bbf8bc59
Billy Campbell <wcampbell91@gmail.com>
2020-08-12 14:39:25 -0500

    added styling and killAStudent works

--- src/components/SharkTank/SharkTank.js
+++ src/components/SharkTank/SharkTank.js
@@ -2,10 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import studentsShape from '../../helpers/props/studentsShape';
 import LiveStudent from '../LiveStudent/LiveStudent';
+import './SharkTank.scss';
 
 class SharkTank extends React.Component {
   static propTypes = {
     livingStudents: PropTypes.arrayOf(studentsShape.studentsShape),
+    killAStudent: PropTypes.func,
+  }
+
+  killStudentEvent = (e) => {
+    e.preventDefault();
+    const { livingStudents, killAStudent } = this.props;
+    const randomNumber = Math.floor(Math.random() * Math.floor(5));
+    console.error(randomNumber);
+
+    livingStudents.forEach((student) => {
+      if (student.id === `student${randomNumber}`) {
+        killAStudent(student.id);
+      }
+    });
   }
 
   render() {
@@ -16,10 +31,12 @@ class SharkTank extends React.Component {
     ));
 
     return (
-      <div>
-        <h2>Shark Tank!</h2>
-        { livingStudentCards }
-        <button className="btn btn-danger">SHARK ATTACK</button>
+      <div className="sharkTank container display-flex flex-wrap justify-content-center">
+        <h2 className="mt-2">Shark Tank!</h2>
+        <div className="row justify-content-center">
+          { livingStudentCards }
+        </div>
+        <button className="btn btn-danger mb-4 mt-4" onClick={this.killStudentEvent}>SHARK ATTACK</button>
       </div>
     );
   }
